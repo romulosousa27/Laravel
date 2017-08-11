58aff26b23635f4c7e0db49c320dabe3c189a7f8
Romulo Sousa <romulosousa27@hotmail.com>
2017-08-11 09:17:41 -0300

    trabalhando com CRUD e banco de dados

--- projeto_laravel/app/Http/Controllers/Painel/ProdutoController.php
+++ projeto_laravel/app/Http/Controllers/Painel/ProdutoController.php
@@ -4,82 +4,77 @@ namespace App\Http\Controllers\Painel;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Painel\Product;
 
-class ProdutoController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return  'Listagem dos produtos';
+class ProdutoController extends Controller{
+    
+    private $product;
+    
+    public function __construct(Product $product) {
+        $this->product = $product;
+    }
+    
+    public function index(){
+        $products = $this->product->all();
+        return view('painel.products.index', compact('products'));
+    }
+    
+    public function create(){
+        
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function store(Request $request){
+        
+    }
+    
+    public function show($id){
+        
+    }
+    
+    public function edit($id){
+        
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    public function update(Request $request, $id){
+        
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
+    public function destroy($id){
+        
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+    public function tests(){
+    
+    /*
+        $prod = $this->product;
+        $prod->name = 'Nome do produto';
+        $prod->number = 763;
+        $prod->active = true;
+        $prod->category = 'eletronica';
+        $prod->description = 'Descrição do produto';
+        $insert = $prod->save();
+        
+        if($insert){
+            return 'Produto inserido com sucesso';
+        }
+        else{
+            return 'Erro ao inserir!';
+        }
+    */
+        
+        $insert = $this->product->insert([
+            'name' => 'Nome do produto 2',
+            'number' => '132421',
+            'active' => FALSE,
+            'category' => 'eletronica',
+            'description' => 'Descrição vem aqui'
+        ]);
+        
+        if($insert){
+            return 'Produto inserido com sucesso';
+        }
+        else{
+            return 'Erro ao inserir!';
+        }
+        
     }
 }
--- projeto_laravel/routes/web.php
+++ projeto_laravel/routes/web.php
@@ -1,22 +1,11 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 /*  Rota padrão 
 	Route::get('/', function () {
     	return view('welcome');
 	});
 */
 
+Route::get('/painel/produtos/tests','Painel\ProdutoController@tests');
 Route::resource('/painel/produtos','Painel\ProdutoController');
 
 Route::group(['namespace' => 'Site'], function(){
